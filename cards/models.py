a803f8783c66e665878e886bf78cdf62f8257172
Instelce <instelce@protonmail.com>
2022-12-02 10:31:46 +0100

    Adding login and logout

--- cards/models.py
+++ cards/models.py
@@ -4,33 +4,33 @@ from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
 
 
-class CardGroup(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+# class CardGroup(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    name = models.CharField(max_length=256)
-    description = models.TextField(max_length=600)
-    lesson = models.TextField(default="")
+#     name = models.CharField(max_length=256)
+#     description = models.TextField(max_length=600)
+#     lesson = models.TextField(default="")
 
-    slug = models.SlugField(unique=True, max_length=100)
-    tags = TaggableManager()
+#     slug = models.SlugField(unique=True, max_length=100)
+#     tags = TaggableManager()
 
-    visibility = models.BooleanField(default=False)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
-    unlock_password = models.IntegerField(max_length=8, default=0)
+#     visibility = models.BooleanField(default=False)
+#     created_at = models.DateField(auto_now_add=True)
+#     updated_at = models.DateField(auto_now=True)
+#     unlock_password = models.IntegerField(max_length=8, default=0)
 
-    def __str__(self):
-        return f"{self.name} Group - {self.user.username}"
+#     def __str__(self):
+#         return f"{self.name} Group - {self.user.username}"
 
 
-class Card(models.Model):
-    group = models.ForeignKey(CardGroup, on_delete=models.CASCADE)
+# class Card(models.Model):
+#     group = models.ForeignKey(CardGroup, on_delete=models.CASCADE)
 
-    term = models.CharField(max_length=256)
-    definition = models.TextField(max_length=600)
-    favorite = models.BooleanField(default=False)
-    is_learned = models.BooleanField(default=False)
+#     term = models.CharField(max_length=256)
+#     definition = models.TextField(max_length=600)
+#     favorite = models.BooleanField(default=False)
+#     is_learned = models.BooleanField(default=False)
 
-    def __str__(self):
-        return f"{self.group.name} - {self.term} Card"
+#     def __str__(self):
+#         return f"{self.group.name} - {self.term} Card"
 
--- src/urls.py
+++ src/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 from users import views as users_views
 
@@ -10,7 +11,9 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('cards.urls')),
     
-    path("register/", users_views.register, name="register"),
+    path("sign-in", users_views.register, name="sign-in"),
+    path("login", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
+    path("logout", auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
 
     path("__reload__/", include("django_browser_reload.urls"))
 ] 
