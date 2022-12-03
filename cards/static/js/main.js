69cf5aa03a271b6a943eff285e1d66c4c5fcbeb7
Instelce <instelce@protonmail.com>
2022-12-03 15:56:51 +0100

    Stilization of forms

---
+++ cards/static/js/main.js
@@ -0,0 +1,10 @@
+let inputs = document.querySelectorAll('input')
+let textareas = document.querySelectorAll('textarea')
+
+inputs.forEach(input => {
+    input.placeholder = " "
+});
+
+textareas.forEach(textarea => {
+    textarea.placeholder = " "
+});
---
+++ cards/static/js/scroll.js
@@ -0,0 +1,4 @@
+// const locoScroll = new LocomotiveScroll({
+//     el: document.querySelector(".scroll-container"),
+//     smooth: true
+// })
